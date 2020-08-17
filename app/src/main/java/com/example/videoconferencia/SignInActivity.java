29a82e3bc86625a943edd4bfeaf004a40d01cca2
ElvisDiazLara <46111283+ElvisDiazLara@users.noreply.github.com>
2020-08-17 13:51:47 -0500

    Creacion inicio de sesion y registro xml

---
+++ app/src/main/java/com/example/videoconferencia/SignInActivity.java
@@ -0,0 +1,23 @@
+package com.example.videoconferencia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+        findViewById(R.id.textSingUp).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), SignUpActivity.class));
+            }
+        });
+    }
+}
